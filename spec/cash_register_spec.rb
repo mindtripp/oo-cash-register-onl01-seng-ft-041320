8ff8ddf8485dfacc57ef3d51ead6a589867a9181
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-23 19:41:08 +0000

    Automatically backed up by Learn

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -43,7 +43,7 @@ describe 'CashRegister' do
       it 'applies the discount to the total price' do
         cash_register_with_discount.add_item("macbook air", 1000)
         cash_register_with_discount.apply_discount
-        expect(cash_register_with_discount.total).to eq($800)
+        expect(cash_register_with_discount.total).to eq(800)
       end
 
       it 'returns success message with updated total' do
