abb0a4bd14f753167f818ec705db9789c985dd37
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-23 19:36:19 +0000

    Automatically backed up by Learn

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -71,21 +71,4 @@ describe 'CashRegister' do
       new_register.add_item("eggs", 1.99)
       new_register.add_item("tomato", 1.76, 3)
       expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
-    end
-  end
-
-  describe '#void_last_transaction' do
-    it 'subtracts the last item from the total' do
-      cash_register.add_item("apple", 0.99)
-      cash_register.add_item("tomato", 1.76)
-      cash_register.void_last_transaction
-      expect(cash_register.total).to eq(0.99)
-    end
-
-    it 'returns the total to 0.0 if all items have been removed' do
-      cash_register.add_item("tomato", 1.76, 2)
-      expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(3.52).to(0.0)
-    end
-  end
-end
-
+   
